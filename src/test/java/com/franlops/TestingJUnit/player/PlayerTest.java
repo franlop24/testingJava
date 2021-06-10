9c438db2afdda2e2470767d4daa62600ef41cf31
Francisco Lopez <franlopbri@gmail.com>
2021-06-10 14:22:07 -0500

    Test con Mockito para simular un dado

---
+++ src/test/java/com/franlops/TestingJUnit/player/PlayerTest.java
@@ -0,0 +1,33 @@
+package com.franlops.TestingJUnit.player;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+    public void looses_when_dice_number_is_too_low(){
+
+        //Dice dice = new Dice(6);
+        Dice dice = new Mockito().mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+
+        Player player = new Player(dice, 3);
+        //assertEquals(false, player.play());
+        assertFalse(player.play());
+
+
+    }
+    @Test
+    public void looses_when_dice_number_is_big(){
+
+        //Dice dice = new Dice(6);
+        Dice dice = new Mockito().mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+
+    }
+}
