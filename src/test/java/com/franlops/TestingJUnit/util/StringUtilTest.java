1f747172915a305b1337eaed9dc0ab1c8e81e031
Francisco Lopez <franlopbri@gmail.com>
2021-06-10 13:59:36 -0500

    Organizacion de test con JUnit

--- src/test/java/com/franlops/TestingJUnit/util/StringUtilTest.java
+++ src/test/java/com/franlops/TestingJUnit/util/StringUtilTest.java
@@ -7,15 +7,24 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat(){
-
-        //Test 1
-        String result = StringUtil.repeat("hola", 3);
-        Assert.assertEquals("holaholahola", result);
-
-        //Test2
-        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    public void repeat_string_once(){
+        String result = StringUtil.repeat("hola", 1);
+        Assert.assertEquals("hola", result);
     }
 
+    @Test
+    public void repeat_string_multiple_times(){
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
 
+    @Test
+    public void repeat_string_sero_times(){
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repear_string_negative_times(){
+        Assert.assertEquals("", StringUtil.repeat("hola", -1));
+
+    }
 }
