32900f42847c7f3c5ab2d2086053bd5ce5440d19
Francisco Lopez <franlopbri@gmail.com>
2021-06-10 13:09:09 -0500

    crea test unitario - lanzar exception sobre error

--- src/test/java/com/franlops/TestingJUnit/util/StringUtilTest.java
+++ src/test/java/com/franlops/TestingJUnit/util/StringUtilTest.java
@@ -4,16 +4,15 @@ package com.franlops.TestingJUnit.util;
 
 public class StringUtilTest {
     public static void main(String[] args) {
-        String result = StringUtil.repeat("hola", 2);
+        String result = StringUtil.repeat("hola", 3);
+        assertEquals(result, "holaholahola");
 
-        if(!result.equals("holaholahola")){
-            System.out.println("ERROR");
-        }
+        assertEquals(StringUtil.repeat("hola", 1), "hola");
+    }
 
-        String result2 = StringUtil.repeat("hola", 2);
-
-        if(!result2.equals("hola")){
-            System.out.println("ERROR");
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to expected " + expected);
         }
     }
 }
