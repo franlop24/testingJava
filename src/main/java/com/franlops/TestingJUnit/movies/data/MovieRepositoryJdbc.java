aaead65d833ea34603bb8ff50bbf38928b44a7f8
Francisco Lopez <franlopbri@gmail.com>
2021-06-16 20:25:40 -0500

    test de integracion - guardar peliculas y buscar por id

--- src/main/java/com/franlops/TestingJUnit/movies/data/MovieRepositoryJdbc.java
+++ src/main/java/com/franlops/TestingJUnit/movies/data/MovieRepositoryJdbc.java
@@ -17,7 +17,10 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public Movie findById(long id) {
-        return null;
+
+        Object[] args = { id };
+
+        return jdbcTemplate.queryForObject("select * from movies where id = ?", args, moviMapper);
     }
 
     @Override
@@ -28,7 +31,9 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public void saveOrUpdate(Movie movie) {
-
+        jdbcTemplate.update("insert into movies (name, minutes, genre) values (?, ?, ?)",
+                movie.getName(), movie.getMinutes(), movie.getGenre().toString()
+                );
     }
 
     private static RowMapper<Movie> moviMapper = (rs, rowNum) -> new Movie(
--- src/test/java/com/franlops/TestingJUnit/movies/data/MovieRepositoryIntegrationTest.java
+++ src/test/java/com/franlops/TestingJUnit/movies/data/MovieRepositoryIntegrationTest.java
@@ -3,6 +3,8 @@ package com.franlops.TestingJUnit.movies.data;
 import com.franlops.TestingJUnit.movies.model.Genre;
 import com.franlops.TestingJUnit.movies.model.Movie;
 import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,6 +14,7 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 import javax.sql.DataSource;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -19,16 +22,23 @@ import static org.junit.Assert.*;
 
 public class MovieRepositoryIntegrationTest {
 
-    @Test
-    public void load_all_movies() throws SQLException {
-        DataSource dataSource =
+    private MovieRepositoryJdbc movieRepository;
+    private DriverManagerDataSource dataSource;
+
+    @Before
+    public void setUp() throws Exception {
+        dataSource =
                 new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
 
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-        MovieRepositoryJdbc movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+        movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+    }
+
+    @Test
+    public void load_all_movies() throws SQLException {
 
         Collection<Movie> movies = movieRepository.findAll();
 
@@ -37,5 +47,33 @@ public class MovieRepositoryIntegrationTest {
                 new Movie(2, "Memento", 113, Genre.THRILLER),
                 new Movie(3, "Matrix", 136, Genre.ACTION)
         )));
+    }
+
+    @Test
+    public void load_movie_by_id() {
+
+        Movie movie = movieRepository.findById(2);
+
+        assertThat(movie, CoreMatchers.is(new Movie(2, "Memento", 113, Genre.THRILLER)));
+    }
+
+    @Test
+    public void insert_a_movie() {
+
+        Movie movie = new Movie("Super 8", 112, Genre.THRILLER);
+
+        movieRepository.saveOrUpdate(movie);
+
+        Movie movieFromDb = movieRepository.findById(4);
+
+        assertThat(movieFromDb, CoreMatchers.is(new Movie(4,"Super 8", 112, Genre.THRILLER)));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        // Remove H2 files -- https://stackoverflow.com/a/51809831/1121497
+        final Statement s = dataSource.getConnection().createStatement();
+        s.execute("drop all objects delete files"); // "shutdown" is also enough for mem db
     }
 }
