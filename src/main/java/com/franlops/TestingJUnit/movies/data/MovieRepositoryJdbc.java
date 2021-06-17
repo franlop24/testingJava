87f76a82b54c9680c865c5707cb9c44ff2729dc3
Francisco Lopez <franlopbri@gmail.com>
2021-06-16 20:08:56 -0500

    creacion de la DB y test de integracion

---
+++ src/main/java/com/franlops/TestingJUnit/movies/data/MovieRepositoryJdbc.java
@@ -0,0 +1,40 @@
+package com.franlops.TestingJUnit.movies.data;
+
+import com.franlops.TestingJUnit.movies.model.Genre;
+import com.franlops.TestingJUnit.movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.Collection;
+
+public class MovieRepositoryJdbc implements MovieRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MovieRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+
+        return jdbcTemplate.query("select * from movies", moviMapper);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+
+    private static RowMapper<Movie> moviMapper = (rs, rowNum) -> new Movie(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getInt("minutes"),
+            Genre.valueOf(rs.getString("genre"))
+    );
+}
--- src/main/java/com/franlops/TestingJUnit/movies/model/Movie.java
+++ src/main/java/com/franlops/TestingJUnit/movies/model/Movie.java
@@ -2,6 +2,8 @@ package com.franlops.TestingJUnit.movies.model;
 
 import com.franlops.TestingJUnit.util.StringUtil;
 
+import java.util.Objects;
+
 public class Movie {
 
     private Integer id;
@@ -34,5 +36,21 @@ public class Movie {
 
     public Genre getGenre() {
         return genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return minutes == movie.minutes &&
+                Objects.equals(id, movie.id) &&
+                Objects.equals(name, movie.name) &&
+                genre == movie.genre;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, minutes, genre);
     }
 }
